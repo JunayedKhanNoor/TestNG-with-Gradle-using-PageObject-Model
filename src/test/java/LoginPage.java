3a5b8a1a3039a08467656792ecbef89f9ff2aee3
163002029.junayed <junayednoor@gmail.com>
2023-07-12 12:27:30 -0400

    Login test OrangeHRM

---
+++ src/test/java/LoginPage.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    @FindBy(name = "username")
+    WebElement txtUserName;
+    @FindBy(name = "password")
+    WebElement txtPassword;
+    @FindBy(css = "[type=submit]")
+    WebElement btnSubmit;
+    public  LoginPage (WebDriver driver){
+        PageFactory.initElements(driver,this);
+    }
+    public void doLogin(String username, String password){
+        txtUserName.sendKeys(username);
+        txtPassword.sendKeys(password);
+        btnSubmit.click();
+    }
+}
